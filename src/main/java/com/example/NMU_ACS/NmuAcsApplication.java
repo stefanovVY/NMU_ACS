40ba3acea6877fd05efee36e701c5b6a98bd4140
Viktor <v.y.stefanov@gmail.com>
2023-06-21 03:46:30 +0300

    First

---
+++ src/main/java/com/example/NMU_ACS/NmuAcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.NMU_ACS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmuAcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmuAcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NMU_ACS/NmuAcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NMU_ACS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmuAcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
